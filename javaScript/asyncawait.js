09a00e88a6d9f713a0aebd0208b6c38897dca451
tornadohead <sieber.thomas@yahoo.com>
2022-03-23 17:49:11 +0100

    new from tower

--- javaScript/asyncawait.js
+++ javaScript/asyncawait.js
@@ -13,10 +13,25 @@ somePromise = new Promise((resolve, reject)=>{
 async function afn(){
 	let result = await somePromise;
 	console.log(result)
+	return result
 }
 
 console.log("1st call")
 
-
-
 afn()
+
+////////////////////////////////////////////////////////////////////////////////
+
+//await wartet auch auf Funktionen, die mit async markiert wurden
+//aber kein Promise zurück gebe.
+//
+//Beispiel:
+
+async function afn2(){
+	//solange afn nicht vollständig ausgeführt wurde, wird afn2 vom
+	//call-stack genommen.
+	let someText = await afn();
+	console.log("this only call, if afn is executed")
+}
+
+afn2()
